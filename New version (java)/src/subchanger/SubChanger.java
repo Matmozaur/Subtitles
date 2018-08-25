32a5976b81ce4503f3e95a54bb510110903db85f
Matmozaur <matt950806@wp.pl>
2018-08-25 16:05:13 +0200

    Gui for java version

---
+++ New version (java)/src/subchanger/SubChanger.java
@@ -0,0 +1,21 @@
+package subchanger;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+
+public class SubChanger {
+
+    
+    public static void main(String[] args) {       
+        System.out.print("sadasd");
+         EventQueue.invokeLater(() -> {
+             SubchangerGUI frame = new SubchangerGUI();
+             frame.setTitle("TextComponentTest");
+             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+             frame.setBounds(10, 10, 600, 400);
+             frame.setVisible(true);
+         });
+    }
+    
+}
